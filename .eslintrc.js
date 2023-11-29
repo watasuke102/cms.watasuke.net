971e2ca6e555e50e8c0608b9d37afcc24c7aa3af
watasuke102 <watasuke102@gmail.com>
2023-11-30 00:18:02 +0900

    update: change config (eslint, prettier)

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'unused-imports', 'react-hooks'],
+  rules: {
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: 'off',
+    '@typescript-eslint/semi': ['error'],
+    '@typescript-eslint/explicit-module-boundary-types': [
+      'warn',
+      {
+        allowDirectConstAssertionInArrowFunctions: false,
+      },
+    ],
+    'unused-imports/no-unused-imports': 'error',
+    // 割り当てる前に～のエラーが出るのが嫌なので
+    '@typescript-eslint/no-inferrable-types': 'off',
+    // case内の宣言は許してほしい
+    'no-case-declarations': 'off',
+    // 正規表現の確認
+    'no-invalid-regexp': 'error',
+    // ===と!==を強制
+    eqeqeq: 'error',
+  },
+};
