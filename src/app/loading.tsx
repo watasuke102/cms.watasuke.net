6a46f394ae8eea7b41e621808730b1d04a0e69b7
watasuke102 <watasuke102@gmail.com>
2023-12-26 13:40:35 +0900

    add: front>loading.tsx to top (article list) page

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+// cms.watasuke.net
+// CopyRight (c) 2023 watasuke
+//
+// Email  : <watasuke102@gmail.com>
+// Twitter: @Watasuke102
+// This software is released under the MIT or MIT SUSHI-WARE License.
+import React from 'react';
+
+export default function Loading(): JSX.Element {
+  return <span>Loading...</span>;
+}
