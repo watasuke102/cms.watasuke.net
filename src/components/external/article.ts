3b46d040858a24527a09ba58b8ff607f1b3225f5
watasuke102 <watasuke102@gmail.com>
2024-04-24 14:03:28 +0900

    update: package and upstream

--- src/components/external/article.ts
+++ src/components/external/article.ts
@@ -6,8 +6,7 @@
 // This software is released under the MIT or MIT SUSHI-WARE License.
 import {EmbedCard} from './EmbedCard/EmbedCard';
 import {InnerEmbedCard} from './EmbedCard/InnerEmbedCard';
-import {FootnoteDef} from '@watasuke.net/components/feature/Article/FootnoteDef/FootnoteDef';
 import {Link} from '@watasuke.net/components/feature/Article/Link/Link';
 import {TocInArticle} from '@watasuke.net/components/feature/Article/TocInArticle/TocInArticle';
 
-export {EmbedCard, FootnoteDef, InnerEmbedCard, Link, TocInArticle};
+export {EmbedCard, InnerEmbedCard, Link, TocInArticle};
